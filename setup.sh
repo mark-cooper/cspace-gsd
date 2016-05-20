48b3120a1951fbb6526fe1236d0d3b851f42a6de
Mark Cooper <markchristophercooper@gmail.com>
2016-05-19 23:02:38 -0700

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# setup db and run migrations
+rake db:drop
+rake db:create
+rake db:migrate
+
+# load gsd data (must be added to db/data directory)
+rake csv:seed[material,db/data/materials.csv]
+rake csv:seed[material_composition,db/data/material2composition.csv]
+rake csv:seed[material_form,db/data/material2form.csv]
+rake csv:seed[material_process,db/data/material2process.csv]
+rake csv:seed[material_property,db/data/material2property.csv]
+rake csv:seed[vendor,db/data/vendors.csv]
