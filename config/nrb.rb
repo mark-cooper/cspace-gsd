9025ee93fb5b224202db5d65ab837ee4f0235490
Mark Cooper <markchristophercooper@gmail.com>
2016-05-27 12:08:47 -0700

    Use cspace base instance vocabs, resolves #1

--- config/nrb.rb
+++ config/nrb.rb
@@ -6,5 +6,5 @@ Nrb.configure do |config|
   # config.autoload_paths = %w(models services)
 
   config.domain           = "materials.collectionspace.org"
-  config.vocabularies_url = "https://raw.githubusercontent.com/mark-cooper/cspace-application/materials_updates/tomcat-main/src/main/resources/defaults/base-instance-vocabularies.xml"
+  config.vocabularies_url = "https://raw.githubusercontent.com/collectionspace/application/master/tomcat-main/src/main/resources/defaults/base-instance-vocabularies.xml"
 end
